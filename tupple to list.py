08bbe61006c32bf37fb002a4c3bd70aa2e296df2
ipsap378 <ipsapaudel@icloud.com>
2025-09-30 11:33:41 +1000

    Add files via upload

---
+++ tupple to list.py
@@ -0,0 +1,13 @@
+my_tuple = (1, 6.79, False, "hello")
+print("my_tupple :" , my_tuple)
+my_list = [my_tuple]
+my_list.append("banana")
+print("my_list with banana:", my_list)
+print("First element:", my_list[0])
+print("Last element:", my_list[-1])
+print("accsess the values in the list:")
+print("Tuple item 1:", my_tuple[0])
+print("Tuple item 2:", my_tuple[1])
+print("Tuple item 3:", my_tuple[2])
+print("Tuple item 4:", my_tuple[3])
+
